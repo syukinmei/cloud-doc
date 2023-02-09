3f08f6da8aac65b2308d67fc045671064ac13f3b
syukinmei <>
2023-02-09 23:52:18 +0800

    init:开发环境搭建

---
+++ main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');
+
+let mainWindow = null;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 1024,
+        height: 680,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    })
+
+    // 环境判断
+    const urlLocation = isDev ? 'http://localhost:3000' : 'dummyUrl';
+    mainWindow.loadURL(urlLocation);
+})
