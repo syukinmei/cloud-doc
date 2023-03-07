68ae3ee6d74470ef1ae070216090e92f23da4318
syukinmei <347086009@qq.com>
2023-03-07 22:21:42 +0800

    feat:创建TabList组件

---
+++ src/components/TabList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const TabList = ({ files, activeId, unsabeIds, onTabClick, onCloseTab }) => {
+  return <div>TabList</div>;
+};
+
+export default TabList;
