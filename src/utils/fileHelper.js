e023e27a07b91f58e927e5947fdd323438ddaa4f
syukinmei <347086009@qq.com>
2023-07-18 23:55:07 +0800

    feat:封装node-fs模块的部分功能

---
+++ src/utils/fileHelper.js
@@ -0,0 +1,18 @@
+// const fs = window.require("fs").promises;
+
+// const fileHelper = {
+//   // 异步地读取文件的全部内容
+//   readFile: (path) => fs.readFile(path, { encoding: "utf-8" }),
+
+//   // 异步地将数据写入文件
+//   writeFile: (path, content) =>
+//     fs.writeFile(path, content, { encoding: "utf-8" }),
+
+//   // 将 oldPath 处的文件异步重命名为作为 newPath 提供的路径名
+//   renameFile: (oldPath, newPath) => fs.rename(oldPath, newPath),
+
+//   // 异步地删除文件
+//   deleteFile: (path) => fs.unlink(path),
+// };
+
+// export default fileHelper;
